ca1253b5528e9e72c77953dc219537bff9324fe6
Dennis Macharia <dennismacharia4747@gmail.com>
2018-09-26 00:37:08 +0300

    [Chore #160767809] add signup, login and more order tests

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 
 # local imports
 from config import configuration
-from .api.v1.db import init_db
+from .api.v2.db import init_db
 from .api.v1.resources.orders import Get_orders, Orders, Orderbyid
 from .api.v2.resources.auth import Register, Login
 
