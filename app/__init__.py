ef03653907c78f5ac013b70298643bc39fb4c64e
Dennis Macharia <dennismacharia4747@gmail.com>
2018-09-27 04:08:50 +0300

    [Feature #160810276] add edit end point for version 2

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_restful import Api
 from config import configuration
 from .api.v1.resources.orders import Get_orders, Orders, Orderbyid
 from .api.v1.resources.auth import Register, Login
-from .api.v2.resources.orders import Get_ordersv2, Ordersv2
+from .api.v2.resources.orders import Get_ordersv2, Ordersv2, EditOrder
 
 # Inisialize blueprint and flask-restful
 api_blueprint = Blueprint('api', __name__)
@@ -41,3 +41,4 @@ api.add_resource(Register, '/v1/signup')
 api.add_resource(Login, '/v1/login')
 api.add_resource(Get_ordersv2, '/v2/orders')
 api.add_resource(Ordersv2, '/v2/orders')
+api.add_resource(EditOrder, '/v1/orders/<int:order_id>')
--- app/api/v2/resources/orders.py
+++ app/api/v2/resources/orders.py
@@ -92,3 +92,74 @@ class Ordersv2(Resource):
             cur.execute("rollback;")
             print(error)
             return {'Message': 'current transaction is aborted'}, 500
+
+
+class EditOrder(Resource):
+    """docstring for Orders"""
+
+    parser = reqparse.RequestParser()
+
+    parser.add_argument(
+        'item',
+        type=str,
+        required=True,
+        help="Food item is required"
+    )
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="Price is required"
+    )
+    parser.add_argument(
+        'address',
+        type=str,
+        required=True,
+        help="Address is required"
+    )
+    parser.add_argument(
+        'quantity',
+        type=int,
+        required=True,
+        help="Quantity is required"
+    )
+
+    def put(self, order_id):
+
+        data = EditOrder.parser.parse_args()
+        item = data["item"]
+        price = data["price"]
+        quantity = data["quantity"]
+        address = data["address"]
+
+        if not item:
+            return {'Message': 'Food item field is required'}, 400
+        elif not price:
+            return {'Message': 'Price field is required'}, 400
+        elif not quantity:
+            return {'Message': 'Image field is required'}, 400
+        elif not quantity:
+            return {'Message': 'Image field is required'}, 400
+
+        try:
+            conn = db()
+            cur = conn.cursor()
+
+            cur.execute("SELECT * FROM orders WHERE order_id = %(order_id)s",
+                        {'order_id': order_id})
+
+            # check if order exist
+            if cur.fetchone() is None:
+                return {'Message': 'Invalid order id'}
+
+            cur.execute("UPDATE  orders SET food=%s, price=%s, address= %s, quantity= %s WHERE order_id=%s",
+                        (item, price, address, quantity, order_id))
+            conn.commit()
+
+            cur.execute("SELECT * FROM orders")
+            res = cur.fetchone()
+            return {'Order': res}
+        except (Exception, psycopg2.DatabaseError) as error:
+            cur.execute("rollback;")
+            print(error)
+            return {'Message': 'current transaction is aborted'}, 500
