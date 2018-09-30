750f5ed243536ad1c26cf7e1060bf3191b6c7911
Dennis Macharia <dennismacharia4747@gmail.com>
2018-09-30 16:16:50 +0300

    [Bug #160875150] add test to check connection

--- app/tests/v2/base.py
+++ app/tests/v2/base.py
@@ -10,13 +10,23 @@ class TestDb(unittest.TestCase):
         from app import create_app
         self.app = create_app('testing')
         self.client = self.app.test_client()
+        self.user = {
+            'email': 'deno@gmail.com',
+            'password': 'aA12345'
+        }
 
         with self.app.app_context():
             self.db = test_db()
 
-    def test_entry(self):
+    def test_base(self):
         res = self.client.get("/")
-        self.assertEqual(200, res.status_code)
+        self.assertEqual(response.status_code, 200)
+
+    def test_200_login(self):
+        """ test 400 for empty password"""
+        response = self.client.post(
+            '/api/v2/auth/login', data=json.dumps(self.user), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
