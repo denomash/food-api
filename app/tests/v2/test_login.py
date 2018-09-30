dbe96b252469bfc69749385d5787cc07427a1dc8
Dennis Macharia <dennismacharia4747@gmail.com>
2018-09-30 21:11:32 +0300

    [Bug #160875150] trial test

--- app/tests/v2/test_login.py
+++ app/tests/v2/test_login.py
@@ -6,7 +6,7 @@ import json
 import psycopg2
 
 from ... import create_app
-from ...api.v2.fastfood import queries
+from ...api.v2.db import testdb
 
 
 class TestDB(unittest.TestCase):
@@ -16,21 +16,6 @@ class TestDB(unittest.TestCase):
         """Define test variables and initialize app."""
         app = create_app('testing')
         self.client = app.test_client()
-
-        try:
-            self.conn = psycopg2.connect(os.getenv('TEST_DB_URL'))
-            self.cur = self.conn.cursor()
-            self.conn.autocommit = True
-
-            # activate connection cursor
-            self.cur = self.conn.cursor()
-            for query in queries:
-                self.cur.execute(query)
-                self.conn.commit()
-        except (Exception, psycopg2.DatabaseError) as error:
-            print("Database not connected")
-            print(error)
-
         self.user = {
             'email': 'deno@gmail.com',
             'password': ''
@@ -63,59 +48,44 @@ class TestDB(unittest.TestCase):
     def test_400_empty_password(self):
         """ test 400 for empty password"""
         response = self.client.post(
-            '/api/auth/login', data=json.dumps(self.user), content_type='application/json')
+            '/api/v2/auth/login', data=json.dumps(self.user), content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
     def test_400_empty_email(self):
         """ test 400 for empty email"""
         response = self.client.post(
-            '/api/auth/login', data=json.dumps(self.user2), content_type='application/json')
+            '/api/v2/auth/login', data=json.dumps(self.user2), content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
     def test_400_invalid_email(self):
         """ test 400 for invalid email"""
         response = self.client.post(
-            '/api/auth/login', data=json.dumps(self.user3), content_type='application/json')
+            '/api/v2/auth/login', data=json.dumps(self.user3), content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
     def test_400_no_lowercase_in_password(self):
         """ test 400 if password has no lowercase"""
         response = self.client.post(
-            '/api/auth/login', data=json.dumps(self.user4), content_type='application/json')
+            '/api/v2/auth/login', data=json.dumps(self.user4), content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
     def test_400_no_integer_in_password(self):
         """ test 400 if password has no integer"""
         response = self.client.post(
-            '/api/auth/login', data=json.dumps(self.user5), content_type='application/json')
+            '/api/v2/auth/login', data=json.dumps(self.user5), content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
     def test_400_no_uppercase_in_password(self):
         """ test 400 if password has no uppercase"""
         response = self.client.post(
-            '/api/auth/login', data=json.dumps(self.user6), content_type='application/json')
+            '/api/v2/auth/login', data=json.dumps(self.user6), content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
     def test_400_atleast_8_characters(self):
         """ test 400 if password atleast 8 characters"""
         response = self.client.post(
-            '/api/auth/login', data=json.dumps(self.user7), content_type='application/json')
+            '/api/v2/auth/login', data=json.dumps(self.user7), content_type='application/json')
         self.assertEqual(response.status_code, 400)
-
-    def tearDown(self):
-        """teardown all initialized variables."""
-        try:
-            self.conn = psycopg2.connect(os.getenv('TEST_DB_URL'))
-            self.conn.autocommit = True
-
-            # activate connection cursor
-            self.cur = self.conn.cursor()
-            self.cur.execute("DROP TABLE IF EXISTS users")
-            self.conn.commit()
-
-        except (Exception, psycopg2.DatabaseError) as error:
-            print("Database not connected")
-            print(error)
 
 
 # Make the tests conveniently executable
