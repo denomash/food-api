2d271f85c9de9933c4aba4e2953ba1b32f4a5e04
Dennis Macharia <dennismacharia4747@gmail.com>
2018-10-01 11:02:36 +0300

    [Chore #160877598] add test signup

--- app/tests/v2/test_signup.py
+++ app/tests/v2/test_signup.py
@@ -15,7 +15,12 @@ class TestDB(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app('testing')
         self.client = self.app.test_client()
-
+        self.user = {
+            'username': 'deno',
+            'email': 'deno@gmail.com',
+            'password': 'aA123456',
+            'confirm password': 'aA123456'
+        }
         self.user2 = {
             'username': 'deno',
             'email': 'denogmail.com',
@@ -91,6 +96,13 @@ class TestDB(unittest.TestCase):
         response = self.client.post(
             '/api/v2/auth/signup', data=json.dumps(self.user7), content_type='application/json')
         self.assertEqual(response.status_code, 400)
+
+    def test_201_register_successfull(self):
+        """test 201 for loggin successfull"""
+        response = self.client.post(
+            '/api/v2/auth/signup', data=json.dumps(self.user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
 
 
 # Make the tests conveniently executable
