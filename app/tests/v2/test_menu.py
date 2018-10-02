98f544b09b81bc44107d91bbd0dbd145df9ff00e
Dennis Macharia <dennismacharia4747@gmail.com>
2018-10-02 23:58:19 +0300

    [Chore #160877598] test 401 user must be an admin

--- app/tests/v2/test_menu.py
+++ app/tests/v2/test_menu.py
@@ -75,7 +75,6 @@ class TestMenu(unittest.TestCase):
         response = self.client.post(
             '/api/v2/menu', data=json.dumps(self.food), headers=headers)
         if response and current_user['type'] != 'admin':
-            self.assertEqual(response['Message'], "Must be an admin")
             self.assertEqual(response.status_code, 401)
 
 
