a1bd318080a0a01bd973fe04c4a8bf5ff8cdeeb9
Dennis Macharia <dennismacharia4747@gmail.com>
2018-10-03 00:40:52 +0300

    [Chore #160877598] test 201 meal added successfully by admin

--- app/tests/v2/test_menu.py
+++ app/tests/v2/test_menu.py
@@ -170,6 +170,25 @@ class TestMenu(unittest.TestCase):
         if response and current_user['type'] == 'admin':
             self.assertEqual(response.status_code, 400)
 
+    def test_201_meal_created_successfully(self):
+        """test 201 meal added successfully by admin"""
+        self.client.post(
+            '/api/v2/auth/signup', data=json.dumps(self.user), content_type='application/json')
+        res = self.client.post(
+            '/api/v2/auth/login', data=json.dumps(self.user1), content_type='application/json')
+        token = json.loads(res.data.decode())['token']
+        data = jwt.decode(token, 'secret')
+        self.cur.execute("SELECT * FROM users WHERE id = %(id)s ",
+                         {'id': data["id"]})
+        current_user = self.cur.fetchone()
+        headers = {
+            'Content-Type': 'application/json',
+            'x-access-token': token}
+        response = self.client.post(
+            '/api/v2/menu', data=json.dumps(self.food), headers=headers)
+        if response and current_user['type'] == 'admin':
+            self.assertEqual(response.status_code, 400)
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
