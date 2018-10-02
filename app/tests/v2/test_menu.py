a3d9f870f0888e421d69affbc5fda61a5c16d7d3
Dennis Macharia <dennismacharia4747@gmail.com>
2018-10-03 00:10:53 +0300

    [Chore #160877598] test 400 empty price field for admin posting anew meal

--- app/tests/v2/test_menu.py
+++ app/tests/v2/test_menu.py
@@ -39,6 +39,11 @@ class TestMenu(unittest.TestCase):
             "price": 500,
             "description": "fried"
         }
+        self.food2 = {
+            "item": "pizza",
+            "price": "",
+            "description": "fried"
+        }
 
         with self.app.app_context():
             self.db = test_db()
@@ -83,7 +88,7 @@ class TestMenu(unittest.TestCase):
             self.assertEqual(response.status_code, 401)
 
     def test_400_no_item(self):
-        """test 400 when admin posts an empty item"""
+        """test 400 when admin posts an empty item field"""
         self.client.post(
             '/api/v2/auth/signup', data=json.dumps(self.user), content_type='application/json')
         res = self.client.post(
@@ -101,7 +106,24 @@ class TestMenu(unittest.TestCase):
         if response and current_user['type'] == 'admin':
             self.assertEqual(response.status_code, 400)
 
-
+    def test_400_no_price(self):
+        """test 400 when admin posts an empty price field"""
+        self.client.post(
+            '/api/v2/auth/signup', data=json.dumps(self.user), content_type='application/json')
+        res = self.client.post(
+            '/api/v2/auth/login', data=json.dumps(self.user1), content_type='application/json')
+        token = json.loads(res.data.decode())['token']
+        data = jwt.decode(token, 'secret')
+        self.cur.execute("SELECT * FROM users WHERE id = %(id)s ",
+                         {'id': data["id"]})
+        current_user = self.cur.fetchone()
+        headers = {
+            'Content-Type': 'application/json',
+            'x-access-token': token}
+        response = self.client.post(
+            '/api/v2/menu', data=json.dumps(self.food2), headers=headers)
+        if response and current_user['type'] == 'admin':
+            self.assertEqual(response.status_code, 400)
 
 
 # Make the tests conveniently executable
