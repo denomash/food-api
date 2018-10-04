a7be12059cf4d5857a393be20e8bb907726fe92c
Dennis Macharia <dennismacharia4747@gmail.com>
2018-10-04 08:58:02 +0300

    [Chore #160979054] test 400 order already exist

--- app/tests/v2/test_oredrs.py
+++ app/tests/v2/test_oredrs.py
@@ -152,10 +152,15 @@ class TestMenu(unittest.TestCase):
             '/api/v2/users/orders', headers=headers)
         self.assertEqual(response.status_code, 404)
 
-        # test 201 user can post a meal
+        # test 201 user can post an order
         response = self.client.post(
             '/api/v2/users/orders', headers=headers, data=json.dumps(self.order))
         self.assertEqual(response.status_code, 201)
+
+        # test 400 order already exist
+        response = self.client.post(
+            '/api/v2/users/orders', headers=headers, data=json.dumps(self.order))
+        self.assertEqual(response.status_code, 400)
 
         # test 200 user order history found
         response = self.client.get(
